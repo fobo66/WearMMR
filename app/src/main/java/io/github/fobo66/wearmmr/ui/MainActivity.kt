fc9b762d4bc26fa03d6ed6e724ba88ea8d9ae3c0
Andrey Mukamolow <fobo66@protonmail.com>
2018-06-16 01:55:16 +0300

    Added logging of the DB exception to Crashlytics

--- app/src/main/java/io/github/fobo66/wearmmr/ui/MainActivity.kt
+++ app/src/main/java/io/github/fobo66/wearmmr/ui/MainActivity.kt
@@ -24,6 +24,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
+import com.crashlytics.android.Crashlytics
 import io.github.fobo66.wearmmr.R
 import io.github.fobo66.wearmmr.db.MatchmakingDatabase
 import io.github.fobo66.wearmmr.util.GlideApp
@@ -107,6 +108,7 @@ class MainActivity : WearableActivity() {
                                 .into(playerPic)
                         }, { error ->
                             Log.e(this.javaClass.name, "Cannot load data from database", error)
+                            Crashlytics.logException(error)
                         })
                 )
             }
