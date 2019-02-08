d4c5074331d1e685046fe25eada6088726e30c4a
Andrey Mukamolov <fobo66@protonmail.com>
2019-02-09 02:14:27 +0300

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "io.github.fobo66.wearmmr"
         minSdkVersion(24)
         targetSdkVersion(28)
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         multiDexEnabled = true
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
