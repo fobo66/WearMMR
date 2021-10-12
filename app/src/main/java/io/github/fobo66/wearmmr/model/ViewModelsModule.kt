dc272f98a1eda7db5e62192ce4c8125042ca8584
Andrey Mukamolov <fobo66@fastmail.com>
2021-10-12 21:20:07 +0300

    Created Koin module for viewmodels

---
+++ app/src/main/java/io/github/fobo66/wearmmr/model/ViewModelsModule.kt
@@ -0,0 +1,20 @@
+package io.github.fobo66.wearmmr.model
+
+import androidx.preference.PreferenceManager
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelsModule = module {
+    viewModel {
+        MainViewModel(
+            preferences = PreferenceManager.getDefaultSharedPreferences(
+                androidContext()
+            )
+        )
+    }
+
+    viewModel {
+        MatchmakingWatchFaceViewModel()
+    }
+}
