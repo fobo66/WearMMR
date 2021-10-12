1a107ec71d339205a79886f8d90954df2bfe9103
Andrey Mukamolov <fobo66@fastmail.com>
2021-10-12 21:20:37 +0300

    Cleaned up import

--- app/src/main/java/io/github/fobo66/wearmmr/App.kt
+++ app/src/main/java/io/github/fobo66/wearmmr/App.kt
@@ -21,7 +21,6 @@ import io.github.fobo66.wearmmr.api.apiModule
 import io.github.fobo66.wearmmr.db.databaseModule
 import io.github.fobo66.wearmmr.model.viewModelsModule
 import io.github.fobo66.wearmmr.util.dispatchersModule
-import net.danlew.android.joda.JodaTimeAndroid
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
