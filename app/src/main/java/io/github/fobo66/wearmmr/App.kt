db0a04b9593c1eeb677fb877a04ff48a206ab3d1
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2021-12-18 00:04:58 +0300

    Planted Timber tree

--- app/src/main/java/io/github/fobo66/wearmmr/App.kt
+++ app/src/main/java/io/github/fobo66/wearmmr/App.kt
@@ -25,11 +25,16 @@ import io.github.fobo66.wearmmr.util.dispatchersModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 @Suppress("unused")
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
         startKoin {
             androidLogger()
