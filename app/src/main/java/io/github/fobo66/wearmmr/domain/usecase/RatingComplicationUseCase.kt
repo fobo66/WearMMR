1ee219d90dc8a9d51af11cf682ba62afb4f720ba
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2021-12-17 03:02:23 +0300

    Drafted usecase implementation

--- app/src/main/java/io/github/fobo66/wearmmr/domain/usecase/RatingComplicationUseCase.kt
+++ app/src/main/java/io/github/fobo66/wearmmr/domain/usecase/RatingComplicationUseCase.kt
@@ -1,10 +1,16 @@
 package io.github.fobo66.wearmmr.domain.usecase
 
-import io.github.fobo66.wearmmr.api.MatchmakingRatingApi
+import io.github.fobo66.wearmmr.api.CoroutinesMatchmakingRatingApi
 import io.github.fobo66.wearmmr.db.MatchmakingDatabase
+import io.github.fobo66.wearmmr.entities.toMatchmakingRating
 
 class RatingComplicationUseCase(
     private val db: MatchmakingDatabase,
-    private val matchmakingRatingClient: MatchmakingRatingApi
+    private val matchmakingRatingClient: CoroutinesMatchmakingRatingApi
 ) {
+    suspend fun execute() {
+        val playerInfo = matchmakingRatingClient.fetchPlayerProfile(1)
+        val rating = playerInfo.toMatchmakingRating()
+        db.coroutinesGameStatsDao().insertRating(rating)
+    }
 }
