1e854fa6bf1af303d9d92197eba988b699b2dd14
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-04-15 22:25:16 +0300

    Load shared assets for the round screen

--- app/src/main/java/io/github/fobo66/wearmmr/ui/MatchmakingRatingSharedAssets.kt
+++ app/src/main/java/io/github/fobo66/wearmmr/ui/MatchmakingRatingSharedAssets.kt
@@ -1,6 +1,7 @@
 package io.github.fobo66.wearmmr.ui
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Paint
 import android.graphics.drawable.BitmapDrawable
 import androidx.core.content.ContextCompat
@@ -14,7 +15,7 @@ class MatchmakingRatingSharedAssets(context: Context) : Renderer.SharedAssets {
         color = ContextCompat.getColor(context, R.color.background)
     }
 
-    val backgroundBitmap = (
+    val backgroundBitmap: Bitmap = (
         ResourcesCompat.getDrawable(
             context.resources,
             R.drawable.dota_logo,
@@ -23,13 +24,21 @@ class MatchmakingRatingSharedAssets(context: Context) : Renderer.SharedAssets {
         ).bitmap
 
     val timeXOffset = context.resources.getDimension(
-        R.dimen.digital_x_offset
+        if (context.resources.configuration.isScreenRound) {
+            R.dimen.digital_x_offset_round
+        } else {
+            R.dimen.digital_x_offset
+        }
     )
 
     val timeYOffset = context.resources.getDimension(R.dimen.digital_y_offset)
 
     val complicationYOffset = context.resources.getDimension(
-        R.dimen.complication_y_offset
+        if (context.resources.configuration.isScreenRound) {
+            R.dimen.complication_y_offset_round
+        } else {
+            R.dimen.complication_y_offset
+        }
     )
 
     val textPaint = Paint().apply {
@@ -38,7 +47,11 @@ class MatchmakingRatingSharedAssets(context: Context) : Renderer.SharedAssets {
         isAntiAlias = true
         color = ContextCompat.getColor(context, R.color.digital_text)
         textSize = context.resources.getDimension(
-            R.dimen.digital_text_size
+            if (context.resources.configuration.isScreenRound) {
+                R.dimen.digital_text_size_round
+            } else {
+                R.dimen.digital_text_size
+            }
         )
     }
 
