4ee78d5c4af4da9f2b912d2a9c28158e38deac75
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-04-22 01:28:14 +0300

    Populated datasource with actual logic

--- app/src/main/java/io/github/fobo66/wearmmr/domain/RatingComplicationDataSource.kt
+++ app/src/main/java/io/github/fobo66/wearmmr/domain/RatingComplicationDataSource.kt
@@ -2,13 +2,19 @@ package io.github.fobo66.wearmmr.domain
 
 import androidx.wear.watchface.complications.data.ComplicationData
 import androidx.wear.watchface.complications.data.ComplicationType
+import androidx.wear.watchface.complications.data.NoDataComplicationData
 import androidx.wear.watchface.complications.data.PlainComplicationText
 import androidx.wear.watchface.complications.data.ShortTextComplicationData
-import androidx.wear.watchface.complications.datasource.ComplicationDataSourceService
 import androidx.wear.watchface.complications.datasource.ComplicationRequest
+import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+import io.github.fobo66.wearmmr.domain.usecase.RatingComplicationUseCase
+import org.koin.android.ext.android.inject
 
-class RatingComplicationDataSource : ComplicationDataSourceService() {
-    override fun getPreviewData(type: ComplicationType): ComplicationData? {
+class RatingComplicationDataSource : SuspendingComplicationDataSourceService() {
+
+    private val ratingComplicationUseCase: RatingComplicationUseCase by inject()
+
+    override fun getPreviewData(type: ComplicationType): ComplicationData {
         return ShortTextComplicationData.Builder(
             PlainComplicationText.Builder("4096").build(),
             PlainComplicationText.Builder("MMR").build()
@@ -16,10 +22,18 @@ class RatingComplicationDataSource : ComplicationDataSourceService() {
             .build()
     }
 
-    override fun onComplicationRequest(
-        request: ComplicationRequest,
-        listener: ComplicationRequestListener
-    ) {
-        TODO("Not yet implemented")
+    override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData {
+        val rating = ratingComplicationUseCase.execute()
+
+        return if (rating != null) {
+            ShortTextComplicationData.Builder(
+                PlainComplicationText.Builder(rating.toString())
+                    .build(),
+                PlainComplicationText.Builder("MMR").build()
+            )
+                .build()
+        } else {
+            NoDataComplicationData()
+        }
     }
 }
