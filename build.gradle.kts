65bd0d00a159b2392525d44567422ac271c3212b
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-06-10 14:46:00 +0300

    Updated Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.0-alpha03")
-        classpath(kotlin("gradle-plugin", version = "1.6.21"))
+        classpath(kotlin("gradle-plugin", version = "1.7.0"))
         classpath("com.google.gms:google-services:4.3.10")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.0")
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.20.0")
