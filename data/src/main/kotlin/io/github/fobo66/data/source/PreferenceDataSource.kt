457ec2493f6e48b36ebadf0cbe7c17d869b1f0b9
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-06-18 12:44:40 +0300

    Initial datasource impl added

--- data/src/main/kotlin/io/github/fobo66/data/source/PreferenceDataSource.kt
+++ data/src/main/kotlin/io/github/fobo66/data/source/PreferenceDataSource.kt
@@ -4,3 +4,13 @@ interface PreferenceDataSource {
     suspend fun getBoolean(key: String, defaultValue: Boolean = false): Boolean
     suspend fun getLong(key: String, defaultValue: Long = 0L): Long
 }
+
+class PreferenceDataSourceImpl : PreferenceDataSource {
+    override suspend fun getBoolean(key: String, defaultValue: Boolean): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getLong(key: String, defaultValue: Long): Long {
+        TODO("Not yet implemented")
+    }
+}
