a8a19d3c3614e186a1dc450ee19f6e87d57f5f38
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-06-26 15:31:56 +0300

    Added datasource implementation

--- data/src/main/kotlin/io/github/fobo66/data/source/PreferenceDataSource.kt
+++ data/src/main/kotlin/io/github/fobo66/data/source/PreferenceDataSource.kt
@@ -1,16 +1,24 @@
 package io.github.fobo66.data.source
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.longPreferencesKey
+import kotlinx.coroutines.flow.first
+
 interface PreferenceDataSource {
     suspend fun getBoolean(key: String, defaultValue: Boolean = false): Boolean
     suspend fun getLong(key: String, defaultValue: Long = 0L): Long
 }
 
-class PreferenceDataSourceImpl : PreferenceDataSource {
+class PreferenceDataSourceImpl(
+    private val datastore: DataStore<Preferences>
+) : PreferenceDataSource {
     override suspend fun getBoolean(key: String, defaultValue: Boolean): Boolean {
-        TODO("Not yet implemented")
+        return datastore.data.first()[booleanPreferencesKey(key)] ?: defaultValue
     }
 
     override suspend fun getLong(key: String, defaultValue: Long): Long {
-        TODO("Not yet implemented")
+        return datastore.data.first()[longPreferencesKey(key)] ?: defaultValue
     }
 }
