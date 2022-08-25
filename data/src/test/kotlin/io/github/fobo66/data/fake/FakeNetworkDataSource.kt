fb3bd30cf22173f1650c318cfc701fa1f0c797fe
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-08-25 23:51:39 +0300

    Speed up repo test

--- data/src/test/kotlin/io/github/fobo66/data/fake/FakeNetworkDataSource.kt
+++ data/src/test/kotlin/io/github/fobo66/data/fake/FakeNetworkDataSource.kt
@@ -2,17 +2,6 @@ package io.github.fobo66.data.fake
 
 import io.github.fobo66.data.entities.PlayerInfo
 import io.github.fobo66.data.source.NetworkDataSource
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.engine.mock.MockEngine
-import io.ktor.client.engine.mock.respond
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.get
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
-import io.ktor.http.appendPathSegments
-import io.ktor.http.headersOf
-import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 class FakeNetworkDataSource : NetworkDataSource, Clearable {
@@ -46,41 +35,13 @@ class FakeNetworkDataSource : NetworkDataSource, Clearable {
         }
     """.trimIndent()
 
-    private val engine = MockEngine {
-        val status = if (it.url.encodedPath.contains('1')) {
-            HttpStatusCode.OK
-        } else {
-            HttpStatusCode.InternalServerError
-        }
-
-        respond(
-            content = response,
-            status = status,
-            headers = headersOf(
-                "Content-Type",
-                "application/json"
-            )
-        )
-    }
-
-    private val api = HttpClient(engine) {
-        install(ContentNegotiation) {
-            json(
-                json = Json {
-                    ignoreUnknownKeys = true
-                },
-                contentType = ContentType.Any
-            )
-        }
+    private val json = Json {
+        ignoreUnknownKeys = true
     }
 
     override suspend fun fetchRating(playerId: Long): PlayerInfo {
         isFetched = true
-        return api.get("https://localhost") {
-            url {
-                appendPathSegments(playerId.toString())
-            }
-        }.body()
+        return json.decodeFromString(PlayerInfo.serializer(), response)
     }
 
     override fun clear() {
