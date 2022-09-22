0faa011218c2196a89d0eb3d2ac399c1f6cbcf60
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-09-22 20:30:06 +0300

    Added Material text input

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,22 +24,6 @@ plugins {
     id("io.gitlab.arturbosch.detekt")
 }
 
-/*
- * Copyright 2018. Andrey Mukamolov
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 val wearableVersion = "2.9.0"
 val wearableWatchfaceVersion = "1.1.1"
 val lifecycleVersion = "2.5.1"
@@ -55,7 +39,6 @@ android {
         versionCode = 7
         versionName = "2.0"
         multiDexEnabled = true
-        vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
--- app/src/main/kotlin/io/github/fobo66/wearmmr/ui/SettingsActivity.kt
+++ app/src/main/kotlin/io/github/fobo66/wearmmr/ui/SettingsActivity.kt
@@ -23,16 +23,17 @@ import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView.BufferType.EDITABLE
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 import androidx.core.content.getSystemService
 import androidx.lifecycle.lifecycleScope
 import androidx.wear.watchface.complications.datasource.ComplicationDataSourceUpdateRequester
+import com.google.android.material.color.DynamicColors
 import io.github.fobo66.wearmmr.databinding.ActivitySettingsBinding
 import io.github.fobo66.wearmmr.domain.RatingComplicationDataSource
 import io.github.fobo66.wearmmr.model.SettingsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySettingsBinding
 
@@ -47,17 +48,18 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DynamicColors.applyToActivityIfAvailable(this)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         lifecycleScope.launchWhenResumed {
-            binding.playerIdInput.setText(
+            binding.playerIdInput.editText?.setText(
                 settingsViewModel.loadPlayerId(),
                 EDITABLE
             )
         }
 
-        binding.playerIdInput.setOnEditorActionListener { _, actionId, _ ->
+        binding.playerIdInput.editText?.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 getSystemService<InputMethodManager>()?.hideSoftInputFromWindow(
                     currentFocus!!.windowToken,
@@ -77,7 +79,7 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     private fun updatePlayerIdPreference() {
-        val playerIdString = binding.playerIdInput.text.toString()
+        val playerIdString = binding.playerIdInput.editText?.text.toString()
         if (playerIdString.isNotBlank()) {
             settingsViewModel.savePlayerId(playerIdString)
         }
