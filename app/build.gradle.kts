4a393512c464aebe26051892f9266e6486d24937
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-09-24 15:45:39 +0300

    Remove legacy lib that is no longer needed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
 
-    compileOnly("com.google.android.wearable:wearable:$wearableVersion")
     implementation("androidx.wear:wear:1.3.0-alpha03")
     implementation("androidx.wear.watchface:watchface:$wearableWatchfaceVersion")
     implementation(
