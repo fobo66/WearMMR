843b8dd2224f736122dcd2b192119a2f23b5b38a
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-10-06 22:11:37 +0300

    For #165: refactored logic for rating state handling

--- domain/src/main/kotlin/io/github/fobo66/domain/usecase/ResolveRatingStateImpl.kt
+++ domain/src/main/kotlin/io/github/fobo66/domain/usecase/ResolveRatingStateImpl.kt
@@ -16,6 +16,7 @@
 
 package io.github.fobo66.domain.usecase
 
+import io.github.fobo66.data.entities.MatchmakingRating
 import io.github.fobo66.data.repositories.RatingRepository
 import io.github.fobo66.data.repositories.SettingsRepository
 import io.github.fobo66.domain.entities.RatingState
@@ -37,24 +38,26 @@ class ResolveRatingStateImpl(
 
             if (playerId > 0) {
                 val rating = ratingRepository.loadRating(playerId)
-                rating?.let {
-                    Timber.d("Loaded rating")
-                    if (it.name.isNullOrEmpty() || it.personaName.isNullOrEmpty()) {
-                        Timber.d("Empty rating, likely player id is not actual")
-                        RatingState.InvalidPlayerId
-                    } else {
-                        RatingState.LoadedRating(
-                            playerName = it.name.orEmpty(),
-                            personaName = it.personaName.orEmpty(),
-                            rating = (it.rating ?: 0).toString(),
-                            avatarUrl = it.avatarUrl.orEmpty()
-                        )
-                    }
-                } ?: RatingState.NoRating
+                Timber.d("Loaded rating for %s", playerId)
+                resolveRatingState(rating)
             } else {
                 Timber.d("No player id found")
                 RatingState.NoPlayerId
             }
         }
     }
+
+    private fun resolveRatingState(rating: MatchmakingRating?) = rating?.let {
+        if (it.name.isNullOrEmpty() || it.personaName.isNullOrEmpty()) {
+            Timber.d("Empty rating, likely player id is not actual")
+            RatingState.InvalidPlayerId
+        } else {
+            RatingState.LoadedRating(
+                playerName = it.name.orEmpty(),
+                personaName = it.personaName.orEmpty(),
+                rating = (it.rating ?: 0).toString(),
+                avatarUrl = it.avatarUrl.orEmpty()
+            )
+        }
+    } ?: RatingState.NoRating
 }
