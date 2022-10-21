7ad0011f41849696ac167634e7f393066bf6906c
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-10-21 20:52:32 +0300

    Added Compose dependencies to the app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,22 +16,6 @@
 
 import io.gitlab.arturbosch.detekt.Detekt
 
-/*
- *    Copyright 2022 Andrey Mukamolov
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -106,12 +90,19 @@ dependencies {
 
     implementation(androidx.core)
     implementation(androidx.activity)
+    implementation(androidx.activity.compose)
     implementation(androidx.lifecycle)
     implementation(androidx.viewmodel)
     implementation(androidx.appstartup)
     implementation(androidx.constraint)
     implementation(androidx.wear)
     implementation(libs.material)
+
+    implementation(compose.foundation)
+    implementation(compose.foundation.wear)
+    implementation(compose.navigation)
+    implementation(compose.preview)
+    implementation(compose.material)
 
     implementation(watchface.core)
     implementation(watchface.complication)
