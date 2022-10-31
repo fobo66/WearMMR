38d3da94f26a118bb223f9c987ce2de1c88a4500
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-10-31 22:34:03 +0200

    Updated Compose

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -50,8 +50,8 @@ dependencyResolutionManagement {
 
         register("compose") {
             version("compiler", "1.3.2")
-            version("regular", "1.3.0-rc01")
-            version("wear", "1.1.0-alpha07")
+            version("regular", "1.4.0-alpha01")
+            version("wear", "1.1.0-beta01")
             library("foundation", "androidx.compose.foundation", "foundation").versionRef("regular")
             library("preview", "androidx.compose.ui", "ui-tooling-preview").versionRef("regular")
             library(
@@ -67,8 +67,8 @@ dependencyResolutionManagement {
             library("wear", "androidx.wear:wear:1.3.0-alpha03")
             version("lifecycle", "2.5.1")
             library("core", "androidx.core:core-ktx:1.9.0")
-            library("activity", "androidx.activity:activity-ktx:1.6.0")
-            library("activity-compose", "androidx.activity:activity-compose:1.7.0-alpha01")
+            library("activity", "androidx.activity:activity-ktx:1.7.0-alpha02")
+            library("activity-compose", "androidx.activity:activity-compose:1.7.0-alpha02")
             library("appstartup", "androidx.startup:startup-runtime:1.1.1")
             library("constraint", "androidx.constraintlayout:constraintlayout:2.1.4")
             library("datastore", "androidx.datastore:datastore-preferences:1.0.0")
