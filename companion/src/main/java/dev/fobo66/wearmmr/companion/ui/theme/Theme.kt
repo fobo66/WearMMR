718e242041a73d97d8052ecaed9036c469468cc7
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-01-17 20:44:28 +0200

    Synced default color scheme with watch app

--- companion/src/main/java/dev/fobo66/wearmmr/companion/ui/theme/Theme.kt
+++ companion/src/main/java/dev/fobo66/wearmmr/companion/ui/theme/Theme.kt
@@ -32,15 +32,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Red200,
+    secondary = Red500,
+    tertiary = Red700
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Red500,
+    secondary = Red700,
+    tertiary = Red200
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
