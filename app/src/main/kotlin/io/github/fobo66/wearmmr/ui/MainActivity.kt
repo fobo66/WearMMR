71b30a05d02a268fecc69b71f5b657f2bd2c6f5d
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-05-18 11:01:52 +0300

    Cleanup

--- app/src/main/kotlin/io/github/fobo66/wearmmr/ui/MainActivity.kt
+++ app/src/main/kotlin/io/github/fobo66/wearmmr/ui/MainActivity.kt
@@ -22,7 +22,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
