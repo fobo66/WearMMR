5d232fd4caa0ae27d2a3b8863f4d47e67e471895
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2024-09-01 10:58:22 +0300

    Updated buildSrc compatibility

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -38,13 +38,13 @@ plugins {
 
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_17
+        jvmTarget = JvmTarget.JVM_21
     }
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
